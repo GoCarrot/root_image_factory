414d12609c1e6c0b22d4a79a68ab1e9519b9025f
Alex Scarborough <alex@teak.io>
2022-02-03 09:57:12 -0800

    Make a terraform change to test the thing.

--- terraform/main.tf
+++ terraform/main.tf
@@ -418,17 +418,6 @@ resource "aws_ssm_parameter" "vmbuilder_role" {
   value = aws_iam_instance_profile.vm_builder.name
 }
 
-resource "aws_ssm_parameter" "connection_arn" {
-  provider = aws.admin
-
-  name = "${local.parameter_prefix}/config/core/github_connection_arn"
-  type = "String"
-
-  description = "ARN of the CodeStar connection to our GitHub account"
-
-  value = aws_codestarconnections_connection.github.arn
-}
-
 resource "aws_codestarconnections_connection" "github" {
   name          = "github-connection"
   provider_type = "GitHub"
